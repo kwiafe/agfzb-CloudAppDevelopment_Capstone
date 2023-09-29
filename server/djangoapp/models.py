b7000f57100605359e164c3bf6384e162465d8e9
kwiafe <owurakun@gmail.com>
2023-09-29 16:22:00 -0400

    added models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -25,3 +25,29 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+from django.db import models
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any other fields you'd like for CarMake here
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    year = models.DateField()
+    # Add any other fields you'd like for CarModel here
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
